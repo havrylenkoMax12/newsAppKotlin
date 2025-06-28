c03b485c171b0ec6b05253e454fad2ec37b58ffc
Havrylenko <maxtopparen@gmail.com>
2025-06-28 21:59:08 +0300

    1.5 added db

--- app/src/main/java/org/havrylenko/vrgsoftapp/data/db/ArticleDao.kt
+++ app/src/main/java/org/havrylenko/vrgsoftapp/data/db/ArticleDao.kt
@@ -11,12 +11,18 @@ import org.havrylenko.vrgsoftapp.models.Article
 @Dao
 interface ArticleDao {
 
-    @Query("SELECT * FROM articles")
-    fun getAllArticles(): List<Article>
+    @Query("SELECT * FROM articles ORDER BY id DESC")
+    suspend fun getAllArticles(): List<Article>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(article: Article)
 
     @Delete
     suspend fun delete(article: Article)
+
+    @Query("SELECT * FROM articles WHERE url = :url LIMIT 1")
+    suspend fun getArticleByUrl(url: String): Article?
+
+    @Query("DELETE FROM articles WHERE url = :url")
+    suspend fun deleteByUrl(url: String)
 }
